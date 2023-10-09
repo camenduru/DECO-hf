8d96dccfc27a788eb645a74e9c02ad95b5a2cbd4
HAL-9001\AGNIV <acalphabeta13@gmail.com>
2023-10-09 18:13:17 +0530

    cleared files

--- utils/mesh_utils.py
+++
@@ -1,6 +0,0 @@
-import trimesh
-
-def save_results_mesh(vertices, faces, filename):
-  mesh = trimesh.Trimesh(vertices, faces, process=False)
-  mesh.export(filename)
-  print(f'save results to {filename}')
